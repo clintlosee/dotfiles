2caf62774b530a6c5b3ad3caa0fcf5aeef943a78
Clint Losee <clint.losee@gmail.com>
2021-12-02 11:25:13 -0700

    Add backup and files initial files. Not working currently.

---
+++ setup/backup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# TESTING CURRENTLY - Not finalized
+
+# # copy Vs-Code files
+# cp  $HOME/Library/Application\
+# Support/Code/User/{keybindings.json,settings.json,spellright.dict} .
+
+# # copy snippets folder
+# cp -r $HOME/Library/Application\ Support/Code/User/snippets .
+
+# # copy list of extensions that currently installed
+# code --list-extensions --show-versions >> ../../vscode/extensions.txt
+
+# # copy other dot files
+# cp  $HOME/{.zshrc,.vimrc} .
